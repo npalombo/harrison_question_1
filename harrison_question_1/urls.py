dab724bc61fc40df0984acd6ee5ae5322e9a7472
Nick Palombo <npalombo@gmail.com>
2014-04-11 19:55:10 -0500

    Added authentication as part of Awesome! step

--- harrison_question_1/urls.py
+++ harrison_question_1/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -11,5 +12,6 @@ urlpatterns = patterns('',
     url(r'^$', 'webapp.views.index', name='index'),
     url(r'^state/(?P<state>\w+)/$', 'webapp.views.detail', name='detail'),
 
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- webapp/views.py
+++ webapp/views.py
@@ -1,12 +1,15 @@
 from geodata import fetch
 
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-
+@login_required
 def index(request):
     return render(request, 'index.html', {'states': fetch.states})
 
 
+@login_required
 def detail(request, state):
     all_geodata = fetch.get_data_for_state(state)
     return render(request, 'detail.html', {'state': state, 'all_geodata': all_geodata})
+
